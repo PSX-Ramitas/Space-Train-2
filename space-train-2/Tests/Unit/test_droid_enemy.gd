4d9ad575582e31546883d19c8c87d582aedd1d43
Jason Yang <116393616+Jason-043@users.noreply.github.com>
2024-10-24 12:00:44 -0700

    test_droid updated

--- space-train-2/Tests/Unit/test_droid_enemy.gd
+++ space-train-2/Tests/Unit/test_droid_enemy.gd
@@ -9,8 +9,9 @@ var _sender = InputSender.new(Input)
 func before_each():
 	_level = add_child_autofree(Level.instantiate())
 	_player = _level.get_node('PlayerSS')
-	_droid1 = _player.get_node('Droid1')
-	_droid2 = _player.get_node('Droid2')
+	_droid1 = _level.get_node('Droid1')
+	_droid2 = _level.get_node('Droid2')
+
 func after_each():
 	_sender.release_all()
 	_sender.clear()
@@ -22,13 +23,19 @@ func test_verify_setup():
 
 
 func test_chase():
+	#get difference between droid and player x positions, wait 3 seconds, then assert that it is smaller than before
 	pass
 
 func test_attack():
+	#move the player into the droid's attacking range and assert that the droid is attacking
 	pass
 
 func test_hurt():
+	#make the player attack the droid and assert that it's health is now lower (similar to chase but with health)
 	pass
 
 func test_killed():
+	_droid1.health = 0
+	wait_seconds(2)
+	assert_true(_droid1 == null, "droid can die")
 	pass

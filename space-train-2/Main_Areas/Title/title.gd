3fb1d99d8b09d2c6c5fc04f75015a73308abaad2
Gabriel <147287432+gadosanjos@users.noreply.github.com>
2024-10-29 18:09:13 -0700

    fixed dependancies

---
+++ space-train-2/Main_Areas/Title/title.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main_Areas/Spaceship (hub)/Scenes/spaceship_(hub).tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()

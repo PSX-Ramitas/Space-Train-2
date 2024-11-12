2054f716909737b41905d7e437584f682cf3f96c
Edgar Rodriguez <pablo845rodriguez@gmail.com>
2024-11-12 00:03:41 -0800

    Finalize testing

--- space-train-2/Entities/player_hitbox.gd
+++ space-train-2/Entities/player_hitbox.gd
@@ -6,24 +6,28 @@ class_name PlayerHitbox
 @onready var player_health_bar: TextureProgressBar = $"../HUD/HealthBar"
 
 signal healthChanged(isHeal: bool, amount: int)
-
+var is_alive
 func _ready() -> void:
-	var initial_health = player_ss.get_health()
-	print("Player Health: ", player_ss.health)
-	player_health_bar.init_health(initial_health)
-	player_health_bar._set_health(initial_health)
+	is_alive = true
+	if PlayerData.health == null or PlayerData.health == 0:
+		PlayerData.health = PlayerData.maxHealth
+	print("Player Health: ", PlayerData.health)
+	player_health_bar.init_health(PlayerData.health)
 
 func take_damage(damageAmount: int):
-	if player_health_bar != null: # added this so game wouldnt crash when player dies -tg
-		var new_health = max(0, player_ss.get_health() - damageAmount)
+	if is_alive: 
+		var new_health = max(0, PlayerData.health - damageAmount)
 		player_ss.health = new_health
 		player_health_bar._set_health(new_health)
+		if new_health == 0:
+			is_alive = false
 		# healthChanged.emit( false, damageAmount)
 
 func heal_health(healAmount: int):
-	var new_health = min(player_ss.maxHealth, player_ss.get_health() + healAmount)
-	player_ss.health = new_health
-	player_health_bar._set_health(new_health)
+	if is_alive:
+		var new_health = min(PlayerData.maxHealth, PlayerData.health + healAmount)
+		player_ss.health = new_health
+		player_health_bar._set_health(new_health)
 	# healthChanged.emit( true, healAmount)
 
 func _on_health_changed(isHeal: bool, amount: int) -> void:

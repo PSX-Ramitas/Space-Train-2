1e5b48e8f76b14af7e1e66fd5e9ee6cfaf59c0b9
Tomas <tomasggck@gmail.com>
2024-11-12 16:08:44 -0800

    test_botWheel.gd

--- space-train-2/Tests/UnitSideScroller/test_botWheel.gd
+++ space-train-2/Tests/UnitSideScroller/test_botWheel.gd
@@ -14,7 +14,7 @@ func before_each():
 	_level = add_child_autofree(Level.instantiate())
 	_player = _level.get_node('PlayerSS')
 	_botWheel = _level.get_node('botWheel')
-	_botWheel2 = _level.get_node('botWheel')
+	_botWheel2 = _level.get_node('botWheel2')
 
 func after_each():
 	_sender.release_all()
@@ -31,5 +31,21 @@ func test_chase():
 	var currDistance = _player.position.x - _botWheel2.position.x
 	assert_true(startDistance > currDistance, "botWheel moved towards player")
 
-func test_no_attack():
-	assert_true(_botWheel.state != "Attack", "botWheel can't attack player out of range")
+func test_botWheel_can_die():
+		await wait_seconds(1)
+		for child in _botWheel.get_children():
+			if (child is Damageable):
+				print(_botWheel.name + " took " + str(50) + " damage")
+				var direction_to_damageable = (_botWheel.global_position - get_parent().global_position)
+				var direction_sign = sign(direction_to_damageable.x)
+				if(direction_sign > 0):
+					child.hit(50, Vector2.RIGHT)
+				elif(direction_sign < 0):
+					child.hit(50, Vector2.LEFT)
+				else:
+					child.hit(50, Vector2.ZERO)
+		await wait_seconds(1)
+		assert_null(_botWheel, 'botWheel did not die')
+
+#func test_no_attack():
+	#assert_true(_botWheel.state != "Attack", "botWheel can't attack player out of range")
